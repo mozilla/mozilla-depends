17fd6ee45027957db46fefa4dcd951d3d6bbcd82
Christiane Ruetten <cr@mozilla.com>
2019-08-01 11:54:45 +0200

    WIP

--- utils/mozdep/detectors/nodejs.py
+++ utils/mozdep/detectors/nodejs.py
@@ -8,6 +8,8 @@ from distutils.spawn import find_executable
 from json import load, loads, dump
 import logging
 from pathlib import Path
+from re import match
+from requests import head
 from shutil import rmtree
 from subprocess import run, PIPE, DEVNULL, CalledProcessError
 from tempfile import mkdtemp, TemporaryDirectory
@@ -29,34 +31,272 @@ class RemoveTmpdir(CleanUp):
             rmtree(tmp_dir)
 
 
+def check_url(url: str) -> bool:
+    r = head(url)
+    return 200 <= r.status_code < 300
+
+
+def normalize_repository(repo: str) -> str:
+    # Try to normalize some of the repo schemes.
+    # Schemes observed so far:
+    # hg+https://
+    # git+http://
+    # git+https://
+    # git+ssh://
+    # git://
+    # http://
+    # github:<githubuser>/<githubrepo>
+    # <githubuser>/<githubrepo>
+
+    # Normalize
+    if len(repo) > 0:
+        logger.debug(f"Normalizing repo string `%s`", repo)
+
+    if repo.startswith("github:"):
+        repo = f"https://github.com/{repo[7:]}"
+
+    if len(repo) > 0 and "://" not in repo:
+        # Probably <githubuser>/<githubrepo>
+        repo = f"https://github.com/{repo}"
+
+    if repo.startswith("git+"):
+        repo = repo[4:]
+
+    if repo.startswith("hg+"):
+        repo = repo[3:]
+
+    if repo is None:
+        repo = ""
+
+    if "github.com" in repo:
+        m = match(r""".*github.com/(.+?)/(.+?)(.git)?$""", repo)
+        if m is not None:
+            repo = f"https://github.com/{m[1]}/{m[2]}"
+        else:
+            logger.warning(f"Repo URL looks like GitHub repo, but does not exist: {repo}")
+
+    if len(repo) > 0:
+        logger.debug("Normalization result: `%s`", repo)
+
+    return repo
+
+
 class NodeError(Exception):
     pass
 
 
+def npm_view(npm_bin: Path, package: Path or str) -> dict:
+    """
+    Call `npm view` on either a local package path or a package ref string.
+    Return result as json.
+
+    :param npm_bin: Path to npm binary
+    :param package: Path to package or str with package spec
+    :return: dict with JSON returned by npm
+    """
+    if type(package) is str:
+        npm_cmd = [str(npm_bin), "view", "--json", package]
+        logger.debug("Running `%s`", " ".join(npm_cmd))
+        p = run(npm_cmd, stdout=PIPE, stderr=PIPE)
+    else:
+        npm_cmd = [str(npm_bin), "view", "--json"]
+        logger.debug("Running `%s` in `%s`", " ".join(npm_cmd), str(package))
+        p = run(npm_cmd, stdout=PIPE, stderr=PIPE, cwd=str(package))
+    view_json = loads(p.stdout.decode("utf-8"))
+    return view_json
+
+
+def npm_audit(npm_bin: Path, package: Path) -> dict:
+    """
+    Call `npm audit` on a local package path. Path must contain lock file.
+    Return result as json.
+
+    :param npm_bin: Path to npm binary
+    :param package: Path to package
+    :return: dict with JSON returned by npm
+    """
+    npm_cmd = [str(npm_bin), "audit", "--json"]
+    logger.debug("Running `%s` in `%s`", " ".join(npm_cmd), str(package))
+    p = run(npm_cmd, stdout=PIPE, stderr=PIPE, cwd=str(package))
+    audit_json = loads(p.stdout.decode("utf-8"))
+    return audit_json
+
+
+def npm_list(npm_bin: Path, package: Path) -> dict:
+    """
+    Call `npm list` on a local package path. Path must contain lock file.
+    Return result as json.
+
+    :param npm_bin: Path to npm binary
+    :param package: Path to package
+    :return: dict with JSON returned by npm
+    """
+    npm_cmd = [str(npm_bin), "list", "--long", "--json"]
+    logger.debug("Running `%s` in `%s`", " ".join(npm_cmd), str(package))
+    p = run(npm_cmd, stdout=PIPE, stderr=PIPE, cwd=str(package))
+    list_json = loads(p.stdout.decode("utf-8"))
+    return list_json
+
+
+def yarn_audit(yarn_bin: Path, package: Path) -> list:
+    """
+    Call `yarn audit` on a local package path. Path must contain lock file.
+    Return result as json.
+
+    :param yarn_bin: Path to yarn binary
+    :param package: Path to package
+    :return: list with JSON returned by npm
+    """
+    yarn_cmd = [str(yarn_bin), "audit", "--json"]
+    logger.debug("Running `%s` in `%s`", " ".join(yarn_cmd), str(package))
+    p = run(yarn_cmd, stdout=PIPE, stderr=PIPE, cwd=str(package))
+    audit_json = []
+    for l in p.stdout.decode("utf-8").split("\n"):
+        if len(l) > 1:
+            audit_json.append(loads(l))
+    return audit_json
+
+
 class NodePackage(object):
+
+    _npm_bin = find_executable("npm.exe") or find_executable("npm")
+    _yarn_bin = find_executable("yarn.exe") or find_executable("yarn")
+
     def __init__(self, package: Path):
+        if self._npm_bin is None:
+            raise NodeError("Unable to find npm binary")
+        if self._yarn_bin is None:
+            raise NodeError("Unable to find yarn binary")
         if package.name == "package.json":
             self.dir = package.parent
         elif (package / "package.json").exists():
             self.dir = package
         else:
             raise NodeError(f"Unable to recognize node package at {package}")
-        self._json = None
-        self._lock = None
+        self._json: dict or None = None
+        self._npm_lock: dict or None = None
+        self._yarn_lock: list or None = None
+        self._repository: str or None = None
+        self._npm_view: dict or None = None
+        self._npm_list: dict or None = None
+        self._npm_audit: dict or None = None
+        self._yarn_audit: list or None = None
+        self._str = None
+
+    def is_npm_locked(self):
+        return (self.dir / "package-lock.json").exists() or (self.dir / "npm-shrinkwrap.json").exists()
+
+    def is_yarn_locked(self):
+        return (self.dir / "yarn.lock").exists()
+
+    def is_locked(self):
+        return self.is_npm_locked() or self.is_yarn_locked()
 
     @property
     def json(self):
         if self._json is None:
-            with open(self.dir / "package.json") as f:
+            with (self.dir / "package.json").open() as f:
                 self._json = load(f)
         return self._json
 
     @property
-    def lock(self):
-        if self._lock is None and (self.dir / "package-lock.json").exists():
-            with open(self.dir / "package-lock.json") as f:
-                self._json = load(f)
-        return self._lock
+    def npm_lock(self):
+        if self._npm_lock is None and self.is_npm_locked():
+            if (self.dir / "package-lock.json").exists():
+                with (self.dir / "package-lock.json").open() as f:
+                    self._npm_lock = load(f)
+            elif (self.dir / "npm-shrinkwrap.json").exists():
+                with (self.dir / "npm-shrinkwrap.json").open() as f:
+                    self._npm_lock = load(f)
+            else:
+                raise NodeError(f"Internal error: {self} is marked locked but without lock file")
+        return self._npm_lock
+
+    @property
+    def yarn_lock(self):
+        if self._npm_lock is None and self.is_yarn_locked():
+            with (self.dir / "yarn.lock").open() as f:
+                self._npm_lock = load(f)
+        return self._npm_lock
+
+    def is_private(self):
+        return "private" in self.json and self.json["private"]
+
+    @property
+    def repository(self):
+        if self._repository is None:
+            if "repository" not in self.json:
+                self._repository = ""
+            elif type(self.json["repository"]) is str:
+                self._repository = self.json["repository"]
+            elif type(self.json["repository"]) is dict and "url" in self.json["repository"]:
+                self._repository = self.json["repository"]["url"]
+            else:
+                self._repository = ""
+            self._repository = normalize_repository(self._repository)
+
+        if len(self._repository) == 0:
+            return None
+        else:
+            return self._repository
+
+    @property
+    def npm_view(self) -> dict:
+        if self._npm_view is None:
+            self._npm_view = npm_view(self._npm_bin, self.dir)
+        return self._npm_view
+
+    @property
+    def npm_list(self) -> dict:
+        if self._npm_list is None:
+            self._npm_list = npm_list(self._npm_bin, self.dir)
+        return self._npm_list
+
+    @property
+    def npm_audit(self) -> dict:
+        if self._npm_audit is None:
+            self._npm_audit = npm_audit(self._npm_bin, self.dir)
+        return self._npm_audit
+
+    @property
+    def yarn_audit(self) -> dict:
+        if self._yarn_audit is None:
+            self._yarn_audit = yarn_audit(self._yarn_bin, self.dir)
+        return self._yarn_audit
+
+    def __repr__(self):
+        return f"NodePackage('{str(self.dir)}/package.json')"
+
+    def __hash__(self):
+        return hash(str(self))
+
+    @property
+    def name(self):
+        return self.json["name"] if "name" in self.json else None
+
+    @property
+    def version(self):
+        return self.json["version"] if "version" in self.json else None
+
+    @property
+    def latest_version(self):
+        try:
+            return self.npm_view["dist-tags"]["latest"]
+        except KeyError:
+            return None
+
+    def dependencies(self):
+        if self.is_npm_locked():
+            if "dependencies" in self.npm_lock:
+                for dep in self.npm_lock["dependencies"]:
+                    yield dep, self.npm_lock["dependencies"][dep]["version"]
+
+    def __str__(self):
+        if self._str is None:
+            name = self.name or "_ANONYMOUS"
+            version = self.version or "_UNVERSIONED"
+            self._str = f"{name}@{version} [{str(self.dir)}]"
+        return self._str
 
 
 class NodeEnv(object):
@@ -134,7 +374,80 @@ class NodeEnv(object):
 
 
 def bulk_process(node_env, all_pkgs: Iterable[Path]):
+    """
+    The algorithm for processing node dependencies in the repo:
+
+    For every package.json file in tree:
+    1. Run `npm view` on directory and register as dependency.
+    2. If there's a package-lock.json, call `npm audit` and evaluate result.
+    3. If there's a yarn.lock call `yarn audit` and evaluate result. (TODO)
+    4. If there is no lock file, run `npm install --package-lock-only`
+       in temporary node env and proceed like 2.
+
+    For every external dependency that's not been evaluated so far:
+    1. Call `npm view` and evaluate result, register all dependencies.
+    2. Repeat until all dependencies covered.
+
+    False negatives:
+    - Packages vendored into the tree might have dependencies down the
+      tree that aren't covered yet.
+
+    False positives:
+    - dependencies are registered regardless of their type, and regardless
+      of whether they're actually loaded/used.
+    """
     result = {}
+    unlocked_packages = set()
+    npm_locked_packages = set()
+    yarn_locked_packages = set()
+
+    for p in map(NodePackage, all_pkgs):
+        if p.is_npm_locked():
+            # Handled by `npm view` and `npm audit`
+            npm_locked_packages.add(p)
+            if p.is_yarn_locked():
+                logger.warning(f"Node package {str(p.dir)} is double-locked. "
+                               f"package-lock.json takes precedence over yarn.lock")
+        elif p.is_yarn_locked():
+            # Handled by `yarn view` and `yarn audit`
+            yarn_locked_packages.add(p)
+        else:
+            # Handled by temporary locking and `npm view` and `npm audit`
+            unlocked_packages.add(p)
+
+    logger.info("Node stats: detected %d/%d/%d yarn-locked/npm-locked/unlocked packages",
+                len(yarn_locked_packages), len(npm_locked_packages), len(unlocked_packages))
+
+    for p in yarn_locked_packages:
+        logger.debug("yarn-locked package %s", str(p))
+    for p in npm_locked_packages:
+        logger.debug("npm-locked package %s", str(p))
+        # results = join_results(npm_view_analysis(p), npm_audit_analysis(p))
+    for p in unlocked_packages:
+        logger.debug("unlocked package %s", str(p))
+
+    for p in yarn_locked_packages.union(npm_locked_packages).union(unlocked_packages):
+        deps = set()
+        name = p.name
+        version = p.version
+        repo = p.repository
+        upstream_version = None
+        if not "error" in p.npm_view:
+            if "dist-tags" in p.npm_view:
+                upstream_version = p.npm_view["dist-tags"]["latest"]
+        logger.info("Node dependency: %s@%s (latest: %s) repo: %s", name, version, upstream_version, repo)
+
+    from IPython import embed
+    embed()
+
+    # for p in npm_locked_packages:
+    #     yield {
+    #         "name": p.name,
+    #         "version": p.version,
+    #         "repository": p.repository,
+    #         "upstream_version": p.latest_version
+    #     }
+
     for p in all_pkgs:
         pkg = NodePackage(p)
         if "private" in pkg.json and pkg.json["private"]:
@@ -171,11 +484,11 @@ def bulk_process(node_env, all_pkgs: Iterable[Path]):
 
     for p in all_pkgs:
         pkg = NodePackage(p)
-        if pkg.lock is None:
+        if pkg.npm_lock is None:
             continue
-        for d in pkg.lock["dependencies"]:
+        for d in pkg.npm_lock["dependencies"]:
             dependency_name = d.split("/")[-1]
-            dependency_version = pkg.lock["dependencies"][d]["version"]
+            dependency_version = pkg.npm_lock["dependencies"][d]["version"]
             if dependency_name not in result:
                 logger.warning(f"{p} list dependency {dependency_name} {dependency_version} outside tree")
                 result[dependency_name] = {}
@@ -207,37 +520,36 @@ class NodeDependencyDetector(DependencyDetector):
         return 80
 
     def setup(self) -> bool:
-        if find_executable("npm") is None:
+        npm_bin = find_executable("npm.exe") or find_executable("npm")
+        if npm_bin is None:
             logger.error("Cannot find `npm`")
             return False
+        logger.debug("Using npm executable at %s", npm_bin)
+
+        yarn_bin = find_executable("yarn.exe") or find_executable("yarn")
+        if yarn_bin is None:
+            logger.error("Cannot find `yarn`. Hint: run `npm install -g yarn`")
+            return False
+        logger.debug("Using yarn executable at %s", yarn_bin)
 
         n = NodeEnv()
         logger.debug(f"Created node environment in {n.path}")
 
         self.state = {
-            "node_env": n
+            "node_env": n,
+            "npm_bin": npm_bin,
+            "yarn_bin": yarn_bin
         }
 
         return True
 
     def run(self):
-        # setup_files = list(self.hg.path.glob("third_party/python/*/setup.py"))
-        logger.debug("Scanning for `package.json` files")
+        logger.info("Looking for `package.json` files...")
         package_files = list(self.hg.find("package.json"))
         logger.debug("Found: %s", package_files)
-
-        logger.debug("Scanning for `package-lock.json` files")
-        lock_files = list(self.hg.find("package-lock.json"))
-        logger.debug("Found: %s", lock_files)
-        # Ensure there are no rogue lock files around
-        for l in lock_files:
-            if not (l.parent / "package.json").exists():
-                logger.warning("Found rogue lock file: %s", l)
-
         results = bulk_process(self.state["node_env"], package_files)
-
-        for result in results.items():
-            self.process(result)
+        # for result in results.items():
+        #     self.process(result)
 
     def process(self, arg):
         p, j = arg
